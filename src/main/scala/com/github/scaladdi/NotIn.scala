a4ad6c0a4e84b7cfb968e1c4f4c438d595f3af59
g.wilaszek <g.wilaszek@gmail.com>
2016-01-20 15:42:20 +0100

    fix NotIn

--- src/main/scala/com/github/scaladdi/NotIn.scala
+++ src/main/scala/com/github/scaladdi/NotIn.scala
@@ -7,5 +7,6 @@ trait NotIn[T, L <: HList]
 object NotIn {
   implicit def notInHNil[T] = new NotIn[T, HNil] {}
 
-  implicit def notInList[H, T, L <: HList](implicit ineq: <:!<[T, H], notInTail: NotIn[T, L]) = new NotIn[T, H :: L] {}
+  implicit def notInList[H, T, L <: HList](implicit tIsNotH: <:!<[T, H], hIsNotT: <:!<[T, H], notInTail: NotIn[T, L]) =
+    new NotIn[T, H :: L] {}
 }
