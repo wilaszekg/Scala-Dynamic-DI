6abf96b3890053ebde571ce18388fbb45868ab29
g.wilaszek <g.wilaszek@gmail.com>
2016-05-21 17:57:12 +0200

    generic FutureDependency creation

--- src/main/scala/com/github/wilaszekg/scaladdi/FutureDependency.scala
+++ src/main/scala/com/github/wilaszekg/scaladdi/FutureDependency.scala
@@ -1,6 +1,11 @@
 package com.github.wilaszekg.scaladdi
 
+import shapeless.{Generic, HList}
+import shapeless.ops.function.FnToProduct
+import shapeless.ops.hlist.Tupler
+
 import scala.concurrent.Future
+import scala.language.higherKinds
 
 trait FutureDependency[Args, T] {
   def apply(args: Args): Future[T]
@@ -9,22 +14,21 @@ trait FutureDependency[Args, T] {
 
 object FutureDependency {
 
-  private def create[T, Args](f: Args => Future[T]): FutureDependency[Args, T] = new FutureDependency[Args, T] {
-    override def apply(args: Args): Future[T] = f(args)
+  trait IsFuture[F, T] {
+    def apply(f: F): Future[T]
   }
 
-  def apply[A, R](f: A => Future[R]): FutureDependency[Tuple1[A], R] =
-    new FutureDependency[Tuple1[A], R] {
-      override def apply(args: Tuple1[A]): Future[R] = f(args._1)
+  implicit def isFuture[T] = new IsFuture[Future[T], T] {
+    override def apply(f: Future[T]): Future[T] = f
+  }
+
+  def apply[Args <: HList, FX, X, F, P <: Product](f: F)
+                                              (implicit funProduct: FnToProduct.Aux[F, Args => FX],
+                                               futurize: IsFuture[FX, X],
+                                               tupl: Tupler.Aux[Args, P],
+                                               gen: Generic.Aux[P, Args]): FutureDependency[P, X] =
+    new FutureDependency[P, X] {
+      override def apply(args: P): Future[X] = futurize(funProduct(f)(gen.to(args)))
     }
 
-  def apply[A, B, R](f: (A, B) => Future[R]): FutureDependency[(A, B), R] = create(f.tupled)
-
-  def apply[A, B, C, R](f: (A, B, C) => Future[R]): FutureDependency[(A, B, C), R] = create(f.tupled)
-
-  def apply[A, B, C, D, R](f: (A, B, C, D) => Future[R]): FutureDependency[(A, B, C, D), R] = create(f.tupled)
-
-  def apply[A, B, C, D, E, R](f: (A, B, C, D, E) => Future[R]): FutureDependency[(A, B, C, D, E), R] = create(f.tupled)
-
-  def apply[A, B, C, D, E, F, R](f: (A, B, C, D, E, F) => Future[R]): FutureDependency[(A, B, C, D, E, F), R] = create(f.tupled)
 }
